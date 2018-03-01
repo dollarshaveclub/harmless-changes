fea7a67b311e7b666d15bbd56a3c1c18cd1c7f7d
Jeff Wainwright <yowainwright@gmail.com>
2018-02-28 23:45:29 -0800

    [init] globs minimatch init

--- src/check-for-harmful-changes.js
+++ src/check-for-harmful-changes.js
@@ -4,24 +4,20 @@
   - check that the arguments are defined and are Arrays
   - check if there are potentially harmful changes
 */
+const minimatch = require('minimatch')
 
 module.exports = (changedFilesArray, ciignoreFilesArray, exitStub) => {
-  if (typeof ciignoreFilesArray === 'undefined') {
-    console.error(`Harmless Changes: ${ciignoreFilesArray} is undefined.`)
-    return exitStub()
-  } else if (!Array.isArray(ciignoreFilesArray)) {
-    console.error(`Harmless Changes: ${ciignoreFilesArray} is not an Array.`)
-    return exitStub()
-  } else if (typeof changedFilesArray === 'undefined') {
-    console.error(`Harmless Changes: ${changedFilesArray} is undefined.`)
-    return exitStub()
-  } else if (!Array.isArray(changedFilesArray)) {
-    console.error(`Harmless Changes: ${changedFilesArray} is not an Array.`)
+  try {
+    ciignoreFilesArray.forEach((pattern) => {
+      const match = changedFilesArray.find(file => minimatch(file, pattern, { matchBase: true }))
+      if (match) {
+        console.warn(`Harmless Changes: there are ${potentialHarmfulChangesArray.length} potentially harmful changes. Better Test!`)
+        return exitStub()
+      }
+      return console.warn(`Harmless Changes: there are no harmful changes. Moving on!`)
+    })
+  } catch(err) {
+    console.error(`Harmless Changes: ${err}`)
     return exitStub()
   }
-  const potentialHarmfulChangesArray = changedFilesArray.filter(file => !ciignoreFilesArray.includes(file))
-  if (potentialHarmfulChangesArray.length > 0) {
-    console.warn(`Harmless Changes: there are ${potentialHarmfulChangesArray.length} potentially harmful changes. Better Test!`)
-    return exitStub()
-  } else return console.warn(`Harmless Changes: there are no harmful changes. Moving on!`)
 }
